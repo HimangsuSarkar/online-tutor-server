b747203bc38dbe197c21ac20eeee1f42c486ddcd
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-04-16 01:38:39 +0800

    added code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
+const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const app = express()
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('admins'));
+app.use(fileUpload());
 
 console.log(process.env.DB_USER)
 
@@ -16,7 +19,14 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const tutorCollection = client.db("onlineTutor").collection("tutors");
-    console.log("datebase connect")
+
+    app.post('/addTutor', (req, res) => {
+        const tutor = req.body;
+        tutorCollection.insertOne(tutor)
+            .then(result => {
+                res.send(result.insertedCount);
+            })
+    })
 });
 
 app.get('/', (req, res) => {
